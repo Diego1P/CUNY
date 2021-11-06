a02fbd4b649779fcb35619b3f81fb9559a8732a2
Diego1P <91356775+Diego1P@users.noreply.github.com>
2021-11-06 11:02:07 -0400

    Add files via upload

--- register/models.py
+++ register/models.py
@@ -1,28 +1,20 @@
 from django.db import models
-from datetime import datetime, timedelta
-from django.contrib.auth.models import User
 
-def registraion_period():
-	return datetime.now() + timedelta(days=21)
 
 class Course(models.Model):
-	course_name = models.CharField(max_length=30, default='-')
-	course_id = models.CharField(max_length=6, default='-')
-	instructor = models.ForeignKey(User, on_delete=models.CASCADE)
-	days = models.CharField(max_length=10, default='-')
-	time = models.CharField(max_length=20, default='-')
-	location = models.CharField(max_length=20, default='-')
-	registraion_window = models.DateTimeField(default=registraion_period)
+    course_name = models.CharField(max_length=100)
+    course_id = models.CharField(max_length=100)
+    instructor = models.CharField(max_length=100)
+    day = models.CharField(max_length=100)
+    time = models.CharField(max_length=100)
+    location = models.CharField(max_length=100)
 
-	def __str__(self):
-		return self.course_name
-
-
-# 	
+# 	need to finish the Course model, and add the max length attrib to fields
 #	'course_name' : 'Organic Chemistry 2',
 #	'course_id' : 'CHEM 26300',
 #	'instructor' : 'Mark Biscoe',
 # 	'day' : 'TuThu', 
 #	'time' : '10:00 - 11:50',
 #	'location' : 'online synchronous',
- 
+
+
--- register/views.py
+++ register/views.py
@@ -1,38 +1,53 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
-
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from .forms import UserRegisterForm
 
 classes = [
 
- {
- 	'course_name' : 'Software Engineering',
- 	'course_id' : 'CSC 32200',
- 	'instructor' : 'Jie Wei',
- 	'day' : 'TuThu', 
- 	'time' : '12:00 - 14:15',
- 	'location' : 'online synchronous',
- },
+    {
+        'course_name': 'Software Engineering',
+        'course_id': 'CSC 32200',
+        'instructor': 'Jie Wei',
+        'day': 'TuThu',
+        'time': '12:00 - 14:15',
+        'location': 'online synchronous',
+    },
 
-  {
- 	'course_name' : 'Organic Chemistry 2',
- 	'course_id' : 'CHEM 26300',
- 	'instructor' : 'Mark Biscoe',
- 	'day' : 'TuThu', 
- 	'time' : '10:00 - 11:50',
- 	'location' : 'online synchronous',
- }
-
+    {
+        'course_name': 'Organic Chemistry 2',
+        'course_id': 'CHEM 26300',
+        'instructor': 'Mark Biscoe',
+        'day': 'TuThu',
+        'time': '10:00 - 11:50',
+        'location': 'online synchronous',
+    }
 
 ]
 
+
 def home(request):
-	return render(request, 'register/home.html')
+    return render(request, 'register/home.html')
+
 
 def scenter(request):
+    context = {
+        'classes': classes
+    }
 
-	context = {
-		'classes' : classes
-	}
+    return render(request, 'register/scenter.html', context)
 
-	return render(request, 'register/scenter.html', context)
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Your account has been created! You are now able to log in')
+            return redirect('login')
+    else:
+        form = UserRegisterForm()
+    return render(request, 'register/register.html', {'form': form})
