5f59544e2c45ad55fed91c27e5248669d613b53e
Diego perez <diegopereznn@gmail.com>
2021-11-10 20:04:04 -0500

    fixed an error in views.py, also deleted some fields from form.py

--- register/forms.py
+++ register/forms.py
@@ -6,17 +6,20 @@ from django.forms import SelectDateWidget
 # Choice for the years
 year_choice = [year for year in range(1921, 2022)]
 
+# Choice teacher or student
+TeacherorStudent =(
+    ("teacher", "Teacher"),
+    ("student", "Student"),
+)
 
 class UserRegisterForm(UserCreationForm):
     firstName = forms.CharField(max_length=100)
     lastName = forms.CharField(max_length=100)
     Date_Of_birth = forms.DateField(widget=forms.SelectDateWidget(years=sorted(year_choice, reverse=True)))
-    SSN = forms.CharField(max_length=4,
-                          help_text='Last 4 digits of your Social Security')
+    Teacher_or_Student = forms.ChoiceField(choices=TeacherorStudent)
     email = forms.EmailField(required=True)
-    resume = forms.FileField()
 
     class Meta:
         model = User
-        fields = ['firstName', 'lastName', 'Date_Of_birth', 'SSN', 'username', 'email', 'resume',
+        fields = ['firstName', 'lastName', 'Date_Of_birth', 'Teacher_or_Student', 'username', 'email',
                   'password1', 'password2']
--- register/views.py
+++ register/views.py
@@ -6,48 +6,51 @@ from django.contrib.auth.decorators import login_required
 from .forms import UserRegisterForm
 
 
-
 classes = [
 
- {
- 	'course_name' : 'Software Engineering',
- 	'course_id' : 'CSC 32200',
- 	'instructor' : 'Jie Wei',
- 	'day' : 'TuThu', 
- 	'time' : '12:00 - 14:15',
- 	'location' : 'online synchronous',
- },
+    {
+        'course_name': 'Software Engineering',
+        'course_id': 'CSC 32200',
+        'instructor': 'Jie Wei',
+        'day': 'TuThu',
+        'time': '12:00 - 14:15',
+        'location': 'online synchronous',
+    },
 
-  {
- 	'course_name' : 'Organic Chemistry 2',
- 	'course_id' : 'CHEM 26300',
- 	'instructor' : 'Mark Biscoe',
- 	'day' : 'TuThu', 
- 	'time' : '10:00 - 11:50',
- 	'location' : 'online synchronous',
- }
+    {
+        'course_name': 'Organic Chemistry 2',
+        'course_id': 'CHEM 26300',
+        'instructor': 'Mark Biscoe',
+        'day': 'TuThu',
+        'time': '10:00 - 11:50',
+        'location': 'online synchronous',
+    }
 
 
 ]
 
+
 def home(request):
-	return render(request, 'register/home.html')
+    return render(request, 'register/home.html')
+
 
 def scenter(request):
 
-	context = {
-		'classes' : classes
-	}
+    context = {
+        'classes': classes
+    }
 
-	return render(request, 'register/scenter.html', context)
+    return render(request, 'register/scenter.html', context)
+
 
 def register(request):
     if request.method == 'POST':
-        form = UserRegisterForm(request.POST)
+        form = UserRegisterForm(request.POST, request.FILES)
         if form.is_valid():
-            formbootstrap.save()
+            form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Your account has been created! You are now able to log in')
+            messages.success(
+                request, f'Your account has been created! You are now able to log in')
             return redirect('login')
     else:
         form = UserRegisterForm()
